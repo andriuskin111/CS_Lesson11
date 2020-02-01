4b80bb426db7398d4d6ef41c571a08a3c3c2fffa
andriuskin111 <andriuskin@gmail.com>
2020-02-01 19:08:12 +0200

    Unit Test

--- UMS/UMS.test/UserRepositoryTest.cs
+++ UMS/UMS.test/UserRepositoryTest.cs
@@ -22,5 +22,67 @@ namespace UMS.test
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void TestingUserListRetrieve()
+        {
+            // Arrange
+            UserRepoository userRepoository = new UserRepoository();
+            int expected = 5;
+
+            // Act
+            List<User> userListRetrieved = userRepoository.Retrieve();
+            int actual = userListRetrieved.Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestingUserAdd()
+        {
+            // Arrange
+            UserRepoository userRepoository = new UserRepoository();           
+            string expected = "Vasia";
+
+            // Act
+            userRepoository.AddUser(6, "Vasia", 1);
+            User user = userRepoository.Retrieve(6);
+            string actual = user.UserName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestingUserAddWithMatchUserId()
+        {
+            // Arrange
+            UserRepoository userRepoository = new UserRepoository();
+            string expected = "Vasia";
+
+            // Act
+            userRepoository.AddUser(1, "Gena", 1);
+            User user = userRepoository.Retrieve(1);
+            string actual = user.UserName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestingUserDelete()
+        {
+            // Arrange
+            UserRepoository userRepoository = new UserRepoository();
+            User expected = null;
+
+            // Act
+            userRepoository.DeleteUser(3);
+            User actual = userRepoository.Retrieve(3);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
