bfd9b822e3de1d7a82476dfde02bcab8883996a0
andriuskin111 <andriuskin@gmail.com>
2020-02-06 00:16:33 +0200

    Unit test

--- ND_11_uzduotis2/ND_11_uzduotis2.test/LibraryBookTest.cs
+++ ND_11_uzduotis2/ND_11_uzduotis2.test/LibraryBookTest.cs
@@ -8,8 +8,37 @@ namespace ND_11_uzduotis2.test
     public class LibraryBookTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckOrDelayIfBookReaderWasLate()
         {
+            // Arrange
+            LibraryBook book = new LibraryBook(1, "Roneta", 30);
+
+            book.ReaderName = "Petia";
+            book.DateOfPickUp = new DateTime(2020, 01, 01);
+
+            // Act
+            bool actual = book.CheckOrDelay();
+
+            // Assert
+            Assert.IsTrue(actual);
+
+        }
+
+        [TestMethod]
+        public void CheckOrDelayIfBookReaderNotLate()
+        {
+            // Arrange
+            LibraryBook book = new LibraryBook(1, "Roneta", 30);
+
+            book.ReaderName = "Petia";
+            book.DateOfPickUp = new DateTime(2020, 01, 20);
+
+            // Act
+            bool actual = book.CheckOrDelay();
+
+            // Assert
+            Assert.IsFalse(actual);
+
         }
     }
 }
